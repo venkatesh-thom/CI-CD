bc48e606c18a03457e91d23fc26eabe605f1336c
Venkatesh <tvenky359@gmail.com>
2025-12-29 19:49:42 +0530

    Changes added

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "jenkins_agent" {
   ami                    = local.ami_id
   instance_type          = "t3.small"
   vpc_security_group_ids = [aws_security_group.main.id]
-  subnet_id              = "subnet-01a62adfcf85ac2be" #replace your Subnet
+  subnet_id              = data.aws_subnet.jenkins.id #replace your Subnet
 
   # need more for terraform
   root_block_device {
