39a4eb386f9b37fae58dc94a70c9a3b3f023620f
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-04-24 07:34:02 +0530

    bukcet name updated

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "remote-state-venkatesh-dev"
+    bucket       = "venkatesh-remote"
     key          = "roboshop-dev-jenkins"
     region       = "us-east-1"
     use_lockfile = true
